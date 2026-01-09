420cffe18a8ca130518b748b4af22754a3abd9da
makdadejene <108958177+makdadejene@users.noreply.github.com>
2026-01-09 16:02:26 -0500

    threading

--- src/server.py
+++ src/server.py
@@ -6,21 +6,25 @@ class MiniRedis:
     def __init__(self, host, port):
         self.socket = socket.create_server((host, port), reuse_port=True)
 
-    def ping_pong(self):
+    def ping_pong(self, sock, raddr):
         pong_encode = "+PONG\r\n".encode("utf-8")
         while(True):
-            sock, raddr = self.socket.accept() 
-            while(True):
-                data = sock.recv(1024)
-                if not data:
-                    print(f"Client {raddr} disconnected.")
-                    break
-                sock.sendall(pong_encode)
+            data = sock.recv(1024)
+            if not data:
+                print(f"Client {raddr} disconnected.")
+                break
+            sock.sendall(pong_encode)
+
+    def multi_pong(self):
+        while True:
+            client_sock, client_addr = self.socket.accept()
+            thread = threading.Thread(target=self.ping_pong, args = (client_sock, client_addr))
+            thread.start()
 
 
 def main():
     redis = MiniRedis('localhost', 6379)
-    redis.ping_pong()
+    redis.multi_pong()
 
 
 if __name__ == "__main__":
