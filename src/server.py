ee4e51c1b9507b48af408aa418ac9bcdf844097d
makdadejene <108958177+makdadejene@users.noreply.github.com>
2026-01-09 16:21:42 -0500

    start implementing echo

--- src/server.py
+++ src/server.py
@@ -14,17 +14,24 @@ class MiniRedis:
                 print(f"Client {raddr} disconnected.")
                 break
             sock.sendall(pong_encode)
+        
+    def echo(self, sock, raddr):
+        pass
 
-    def multi_pong(self):
+    def multi_client(self, method):
+        if method == 'ping_pong':
+            target = self.ping_pong
+        elif method == 'echo':
+            target = self.echo
         while True:
             client_sock, client_addr = self.socket.accept()
-            thread = threading.Thread(target=self.ping_pong, args = (client_sock, client_addr))
+            thread = threading.Thread(target=target, args = (client_sock, client_addr))
             thread.start()
 
 
 def main():
     redis = MiniRedis('localhost', 6379)
-    redis.multi_pong()
+    redis.multi_client('echo')
 
 
 if __name__ == "__main__":
